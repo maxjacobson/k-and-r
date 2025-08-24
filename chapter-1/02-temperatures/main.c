2c29d00e89a348442e014fe4d457e11d8644dc79
Max Jacobson <max@hardscrabble.net>
2025-08-24 19:06:58 -0400

    Improve the output

--- chapter-1/02-temperatures/main.c
+++ chapter-1/02-temperatures/main.c
@@ -9,12 +9,12 @@ main()
 
     lower = 0;
     upper = 300;
-    step = 1;
+    step = 20;
 
     fahr = lower;
     while (fahr <= upper) {
         celsius = 5 * (fahr-32) / 9;
-        printf("fahr %d\tcels %d\n", fahr, celsius);
+        printf("%3d\t%6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
