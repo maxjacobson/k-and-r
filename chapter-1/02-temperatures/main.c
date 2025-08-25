134585ecf0b4dbc6578f005717b6148f3f15e4c2
Max Jacobson <max@hardscrabble.net>
2025-08-24 22:03:35 -0400

    display celsius to fahrenheit table too

--- chapter-1/02-temperatures/main.c
+++ chapter-1/02-temperatures/main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
- * for fahr = 0, 20, ... 300 */
-main()
-{
+fahrToCelsius() {
     float fahr, celsius;
     int lower, upper, step;
 
@@ -12,9 +9,38 @@ main()
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheit\tCelsius\n");
+    printf("---------------------\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+}
+
+celsiusToFahrenheit() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 1;
+
+    celsius = lower;
+
+    printf("Celsius\tFahrenheit\n");
+    printf("---------------------\n");
+    while (celsius <= upper) {
+        fahr = (celsius / (5.0/9.0)) + 32.0;
+        printf("%3.0f\t%6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
+
+/* print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ... 300 */
+main()
+{
+    fahrToCelsius();
+    celsiusToFahrenheit();
 }
